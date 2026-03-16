cbd92613565bb15b6fc4347d1b27aa81f6b8ab89
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-03-16 15:16:17 +0530

    Fix CORS

--- src/main/java/com/engineeringdigest/journalApp/config/SecurityConfig.java
+++ src/main/java/com/engineeringdigest/journalApp/config/SecurityConfig.java
@@ -9,9 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.*;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.*;
 
 import java.util.List;
 
@@ -43,10 +41,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "https://journalf.onrender.com"
-        ));
+        config.setAllowedOriginPatterns(List.of("*"));
 
         config.setAllowedMethods(List.of(
                 "GET","POST","PUT","DELETE","OPTIONS"
