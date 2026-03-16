2dc5400cf5ca9caf0cc108e5c0588979ef0a488b
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-03-16 12:18:17 +0530

    add env jwt secret

--- src/main/java/com/engineeringdigest/journalApp/security/JwtUtil.java
+++ src/main/java/com/engineeringdigest/journalApp/security/JwtUtil.java
@@ -2,7 +2,9 @@ package com.engineeringdigest.journalApp.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -11,28 +13,39 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final String SECRET = "journalSecretKeyjournalSecretKeyjournalSecretKey";
+    @Value("${jwt.secret}")
+    private String secret;
 
-    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
     public String generateToken(String username) {
-
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(key)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS384)
                 .compact();
     }
 
     public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
 
-        Claims claims = Jwts.parser()
-                .verifyWith(key)
+    public boolean validateToken(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
-
-        return claims.getSubject();
     }
 }
